44b220eba6a6e2786a0e8f829b7e1870ede6ccf9
Aleksander Stelmaczonek <al.stelmaczonek@gmail.com>
2020-01-29 17:04:32 +0100

    Initialise j.u.l. bridge

--- src/main/kotlin/koriit/kotlin/slf4j/logback/BasicConfiguration.kt
+++ src/main/kotlin/koriit/kotlin/slf4j/logback/BasicConfiguration.kt
@@ -4,16 +4,18 @@ import ch.qos.logback.classic.Level
 import ch.qos.logback.classic.Logger
 import ch.qos.logback.classic.LoggerContext
 import ch.qos.logback.classic.PatternLayout
+import ch.qos.logback.classic.jul.LevelChangePropagator
 import ch.qos.logback.classic.spi.Configurator
 import ch.qos.logback.classic.spi.ILoggingEvent
 import ch.qos.logback.core.ConsoleAppender
 import ch.qos.logback.core.encoder.LayoutWrappingEncoder
 import ch.qos.logback.core.spi.ContextAwareBase
 import net.logstash.logback.encoder.LogstashEncoder
+import org.slf4j.bridge.SLF4JBridgeHandler
 
 /**
  * Overrides default configuration provided by Logback.
- * Changes the pattern to be more useful and pleasant to the human eye.
+ * Changes the pattern to be more useful and pleasant to the human eye. Also configures j.u.l. bridge.
  *
  * This configuration can still be overridden by any `logback-test.xml`, `logback.groovy` and `logback.xml` on the classpath.
  *
@@ -26,14 +28,27 @@ import net.logstash.logback.encoder.LogstashEncoder
  * any NoSQL/JSON Storage should be able to receive such logs.
  */
 class BasicConfiguration : ContextAwareBase(), Configurator {
-    override fun configure(ctx: LoggerContext) {
+    override fun configure(lc: LoggerContext) {
         addInfo("Setting up basic configuration.")
 
+        // Install j.u.l. bridge
+        SLF4JBridgeHandler.removeHandlersForRootLogger()
+        SLF4JBridgeHandler.install()
+
+        // Add LevelChangePropagator to improve j.u.l. logging
+        val propagator = LevelChangePropagator()
+        propagator.context = lc
+        propagator.setResetJUL(true)
+        propagator.start()
+        lc.addListener(propagator)
+
+        // Configuration
         val loggingFormat = System.getProperty("logging.format") ?: System.getenv("LOGGING_FORMAT") ?: "default"
         val loggingLevel = System.getProperty("logging.root.level") ?: System.getenv("LOGGING_ROOT_LEVEL") ?: "INFO"
 
+        // Root appender
         val appender = ConsoleAppender<ILoggingEvent>()
-        appender.context = ctx
+        appender.context = lc
         appender.name = "console"
         appender.encoder = when (loggingFormat.toLowerCase()) {
             "json" -> {
@@ -44,10 +59,10 @@ class BasicConfiguration : ContextAwareBase(), Configurator {
             }
             else -> {
                 LayoutWrappingEncoder<ILoggingEvent>().apply {
-                    context = ctx
+                    context = lc
                     layout = PatternLayout().apply {
                         pattern = "%date{yyyy-MM-dd HH:mm:ss.SSS} %highlight(%-5level) [%-15.15thread] %cyan(%-60(\\(%file:%line\\))) : %message %magenta(%mdc{correlationId} %mdc{subCorrelationId}) %n%xException"
-                        context = ctx
+                        context = lc
                         start()
                     }
                     start()
@@ -56,7 +71,7 @@ class BasicConfiguration : ContextAwareBase(), Configurator {
         }
         appender.start()
 
-        val rootLogger: Logger = ctx.getLogger(Logger.ROOT_LOGGER_NAME)
+        val rootLogger: Logger = lc.getLogger(Logger.ROOT_LOGGER_NAME)
         rootLogger.level = Level.toLevel(loggingLevel, Level.INFO)
         rootLogger.addAppender(appender)
     }
